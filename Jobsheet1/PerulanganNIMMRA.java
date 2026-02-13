2f7361a00e7e5dab83b4a246c6f90e0df78a22d5
Nulsifr-dev <rifqiabdurrahman10@gmail.com>
2026-02-13 19:23:01 +0700

    Praktikum2

--- Jobsheet1/PerulanganNIMMRA.java
+++ Jobsheet1/PerulanganNIMMRA.java
@@ -8,29 +8,31 @@ public class PerulanganNIMMRA {
         System.out.print("Masukkan NIM: ");
         String nim = sc.nextLine();
 
-        int n = Integer.parseInt(nim.substring(nim.length() - 2));
+        char d1 = nim.charAt(nim.length() - 2);
+        char d2 = nim.charAt(nim.length() - 1);
+
+        int n = (d1 - '0') * 10 + (d2 - '0');
 
         if (n < 10) {
             n = n + 10;
         }
 
         for (int i = 1; i <= n; i++) {
-
+            
             if (i == 10 || i == 15) {
                 continue;
             }
             if (i % 3 == 0) {
                 System.out.print("# ");
-            }
-            else if (i % 2 != 0) {
+            } else if (i % 2 != 0) {
                 System.out.print("* ");
-            }
-            else {
+            } else {
                 System.out.print(i + " ");
             }
         }
         sc.close();
+
     }
-    
-    
+
+
 }
